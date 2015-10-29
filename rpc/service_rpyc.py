7b40f8296dd508f0dc79fe5864bbb6eecf9f8a42
walker li <walker8088@gmail.com>
2015-10-29 17:42:24 +0800

    fixd gyro time delta

--- rpc/service_rpyc.py
+++ rpc/service_rpyc.py
@@ -1,4 +1,4 @@
-import sys
+import sys, time
 import rpyc
 
 sys.path.append("..")
@@ -24,13 +24,17 @@ class IMUSensorsService(rpyc.Service):
         self.hmc5883 = HMC5883L(self.i2c)
 	self.mpu6050.init()
 	self.hmc5883.init()
+	self.last_time = time.time()
 
     def exposed_update(self):
 	self.mpu6050.update()
+	new_time = time.time()
+	time_passed = (new_time - self.last_time) * 1000
+	self.last_time = new_time
 	self.hmc5883.update()
 	return (self.mpu6050.accel_scaled_x, self.mpu6050.accel_scaled_y,self.mpu6050.accel_scaled_z,
                 self.mpu6050.gyro_scaled_x, self.mpu6050.gyro_scaled_y, self.mpu6050.gyro_scaled_z,
-                self.hmc5883.scaled_x, self.hmc5883.scaled_y, self.hmc5883.scaled_z ) 
+                self.hmc5883.scaled_x, self.hmc5883.scaled_y, self.hmc5883.scaled_z, time_passed ) 
 
 if __name__ == "__main__":
     from rpyc.utils.server import ThreadedServer
